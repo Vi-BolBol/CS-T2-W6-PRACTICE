d391f802aaf68ce01ef6a34e8bfe4be7894b56d1
BolBol <chhonchhaychanvibol@gmail.com>
2026-02-26 15:51:47 +0700

    Step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import Header from "./components/Header";
 import Scores from "./components/Scores";
-import {HTML_RESULTS} from "./data"
+import * as data from "./data"
 
 function App() {
+
+  const courses = [
+    {index: 0,name: "HTML", res: data.HTML_RESULTS},
+    {index: 1, name: "Java", res: data.JAVA_RESULTS},
+    {index: 2, name: "Python", res: data.PYTHON_RESULTS},
+    {index: 3, name: "English", res: data.ENGLISH_RESULTS}
+  ];
+
   return (
     <>
       <Header batchName="batch 2025"/>
-
       <main className="scores-container">
-        <Scores courseName="HTML" courseResults={HTML_RESULTS}/>
+        {courses.map(i => (
+          <Scores key={i.index} courseName={i.name} courseResults={i.res}/>
+        ))}
       </main>
     </>
   );
