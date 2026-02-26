f9ee59f6bef48555c5c6ea33cae6350bf07cd664
BolBol <chhonchhaychanvibol@gmail.com>
2026-02-26 14:45:22 +0700

    Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import pnLogo from "./assets/pn-logo.png";
 import Header from "./components/Header";
+import Scores from "./components/Scores";
+import {HTML_RESULTS} from "./data"
 
 function App() {
   return (
     <>
-      <Header batchName="fake batch name"/>
+      <Header batchName="batch 2025"/>
 
       <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Scores courseName="HTML" courseResults={HTML_RESULTS}/>
       </main>
     </>
   );
